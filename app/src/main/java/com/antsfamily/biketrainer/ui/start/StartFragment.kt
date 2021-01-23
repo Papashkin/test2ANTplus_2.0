12a00f218ae9a69ac7b6014c79d859dafdf618b9
Pavel Antoshkin <pavel.antoshkin@mooncascade.com>
2021-01-23 19:21:02 +0200

    Redesign: Start page

--- app/src/main/java/com/antsfamily/biketrainer/ui/start/StartFragment.kt
+++ app/src/main/java/com/antsfamily/biketrainer/ui/start/StartFragment.kt
@@ -1,27 +1,39 @@
 package com.antsfamily.biketrainer.ui.start
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.antsfamily.biketrainer.R
+import com.antsfamily.biketrainer.databinding.FragmentStartBinding
 import com.antsfamily.biketrainer.presentation.start.StartViewModel
 import com.antsfamily.biketrainer.presentation.withFactory
 import com.antsfamily.biketrainer.ui.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.fragment_start.*
 
 @AndroidEntryPoint
 class StartFragment : BaseFragment(R.layout.fragment_start) {
 
-    override val viewModel: StartViewModel by viewModels { withFactory(viewModelFactory)  }
+    override val viewModel: StartViewModel by viewModels { withFactory(viewModelFactory) }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentStartBinding.inflate(inflater, container, false).root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setListeners()
+        with(FragmentStartBinding.bind(view)) {
+            bindInteractions(this)
+        }
     }
 
-    private fun setListeners() {
-        btnProfiles.setOnClickListener { viewModel.onProfileClick() }
-        btnPrograms.setOnClickListener { viewModel.onProgramClick() }
+    private fun bindInteractions(binding: FragmentStartBinding) {
+        with(binding) {
+            profilesBtn.setOnClickListener { viewModel.onProfileClick() }
+            programsBtn.setOnClickListener { viewModel.onProgramClick() }
+        }
     }
 }
